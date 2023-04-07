0e4f133914ba809fb2ebb9b2b82185265fb9e265
Nick Qurle <wrl.snk@gmail.com>
2023-04-07 09:56:15 +0300

    tired of thinking up the name of the first commit

--- code.ts
+++ code.ts
@@ -1,52 +1,52 @@
 // Constants
 const confirmMsgs = ["Done!", "You got it!", "Aye!", "Is that all?", "My job here is done.", "Gotcha!", "It wasn't hard.", "Got it! What's next?"]
-const renameMsgs = ["Cleaned", "Affected", "Made it with", "Fixed"]
-const idleMsgs = ["All great, already", "Nothing to do, everything's good", "Any layers to affect? Can't see it", "Nothing to do, your layers are great"]
+const renameMsgs = ["Cleaned", "Affected", "Made it with", "Fixed", "Rounded up"]
+const idleMsgs = ["All great, already", "Nothing to do, everything's good", "Any layers to affect? Can't see it", "Nothing to do, your layers are great", "Round as circle!"]
 
 // Variables
 let notification: NotificationHandler
 let selection: ReadonlyArray<SceneNode>
 let working: boolean
-let count: number = 0
+let nodeCount: number = 0
+let vertCount: number = 0
 
 figma.on("currentpagechange", cancel)
 
-// For networking purposes
-figma.showUI(__html__, { visible: false })
-const post = (k, v = 1, last = false) => figma.ui.postMessage({ k: k, v: v, last: last })
-figma.ui.onmessage = async (msg) => {
-  if (msg === "finished") // Real plugin finish (after server's last response)
-    figma.closePlugin()
-  else
-    console.log(msg)
-}
 
 // Main + Elements Check
-post("started")
 working = true
 selection = figma.currentPage.selection
-
-// Anything selected?
 if (selection.length)
   for (const node of selection)
     mainFunction(node)
-else
-  mainFunction(figma.currentPage)
 finish()
 
 // Action for selected nodes
-function mainFunction(node) {
-  count++
+function mainFunction(node, command = {}, parameters = {}) {
+  if (node.type !== 'VECTOR') return
+  node.x = Math.round(node.x)
+  node.y = Math.round(node.y)
+  let newNetwork = JSON.parse(JSON.stringify(node.vectorNetwork))
+  for (const vert of newNetwork.vertices) {
+    vert.x = Math.round(vert.x)
+    vert.y = Math.round(vert.y)
+    vertCount++
+  }
+  node.vectorNetwork = newNetwork
+  console.log(node.vectorNetwork.vertices)
+  nodeCount++
 }
 
 // Ending the work
 function finish() {
   working = false
   figma.root.setRelaunchData({ relaunch: '' })
-  if (count > 0) {
+  if (nodeCount > 0) {
     notify(confirmMsgs[Math.floor(Math.random() * confirmMsgs.length)] +
       " " + renameMsgs[Math.floor(Math.random() * renameMsgs.length)] +
-      " " + ((count === 1) ? "only one layer" : (count + " layers")))
+      " " + ((nodeCount === 1) ? "only one layer" : (nodeCount + " layers")) +
+      " with " + ((vertCount === 1) ? " one point" : (vertCount + " points")) +
+      " inside")
 
   }
   else notify(idleMsgs[Math.floor(Math.random() * idleMsgs.length)])
